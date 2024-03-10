74577e02bbf5b33399ca9baa4d2245fa57397b40
jitesh-katkar <jiteshkatkar20@gmail.com>
2024-03-10 19:57:36 +0530

    Remove duplicate number from list and array

--- src/EvenOrOddWithoutModulus.java
+++ src/EvenOrOddWithoutModulus.java
@@ -1,12 +1,12 @@
 public class EvenOrOddWithoutModulus {
     public static void main(String[] args) {
 
-        System.out.println(isEvenOrOdd(23));
+        System.out.println(isEvenOrOdd(0));
 
     }
 
     public static boolean isEvenOrOdd(int number){
-        if(number > 0)
+        if(number > 1)
             return (number / 2) * 2 == number;
         else
             return false;
---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+//Remove duplicates from Integer List using java 8 
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+       System.out.println(removeDuplicates(Arrays.asList(12,45,12,45,25,1,2,6,3)));
+       System.out.println("USsing java 8 from array " + removeDuplicatesFromArrayOfInteger(new int[] {12,23,12,25,25,88,54}));
+    }
+
+    public static List<Integer> removeDuplicates(List<Integer> list){
+        return list.stream().distinct().toList();
+
+    }
+
+    
+    public static List<Integer> removeDuplicatesFromArrayOfInteger(int[] list){
+    
+       return Arrays.stream(list).boxed().distinct().toList();
+       
+
+    }
+    
+}
