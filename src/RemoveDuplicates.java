6dcf2c840a1cb6da1ca0c3f32d98b911876105e8
jitesh-katkar <jiteshkatkar20@gmail.com>
2024-03-10 22:07:15 +0530

    program to remove duplicates from list of String

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -7,6 +7,7 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
        System.out.println(removeDuplicates(Arrays.asList(12,45,12,45,25,1,2,6,3)));
        System.out.println("USsing java 8 from array " + removeDuplicatesFromArrayOfInteger(new int[] {12,23,12,25,25,88,54}));
+       System.out.println(removeDuplicatesFromListOfString(Arrays.asList("abc","xyz","abc","add","sub")));
     }
 
     public static List<Integer> removeDuplicates(List<Integer> list){
@@ -19,7 +20,12 @@ public class RemoveDuplicates {
     
        return Arrays.stream(list).boxed().distinct().toList();
        
-
     }
+
+    public static List<String> removeDuplicatesFromListOfString(List<String> list){
+    
+        return list.stream().distinct().toList();
+        
+     }
     
 }
